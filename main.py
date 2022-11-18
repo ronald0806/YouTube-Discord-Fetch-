603941b20c10f019345d2f26ce0129f99064e3bf
Alex <amvandermeulen0234@eagle.fgcu.edu>
2022-11-18 10:49:51 -0500

    added download, play/pause/stop/resume

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import bot
 
+
 if __name__ == '__main__':
     bot.run_discord_bot()
 
--- youtube.py
+++ youtube.py
@@ -1,12 +1,21 @@
-def get_link(message):
-    '''
-    <a id="video-title" class="yt-simple-endpoint style-scope ytd-video-renderer" title="FBI Serves MyPillow CEO Mike Lindell With Search Warrant" aria-label="FBI Serves MyPillow CEO Mike Lindell With Search Warrant by MSNBC 20 hours ago 5 minutes, 34 seconds 175,607 views" href="/watch?v=umqKdAIpu-Q">
-            <yt-icon id="inline-title-icon" class="style-scope ytd-video-renderer" hidden=""><!--css-build:shady--></yt-icon>
-            <yt-formatted-string class="style-scope ytd-video-renderer" aria-label="FBI Serves MyPillow CEO Mike Lindell With Search Warrant by MSNBC 20 hours ago 5 minutes, 34 seconds 175,607 views">FBI Serves MyPillow CEO Mike Lindell With Search Warrant</yt-formatted-string>
-          </a>   
-    '''
+# importing packages
+from pytube import YouTube
+import os
+def download_mp3(url):
+    yt = YouTube(url)
 
-    # download page
-    # find this tag
-    # format string youtube.com/href .. .
-    # return that link
+    # extract only audio
+    video = yt.streams.filter(only_audio=True).first()
+    destination = './videos'
+
+    # download the file
+    out_file = video.download(output_path=destination)
+
+    # save the file
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    return new_file
+
+if __name__ == '__main__':
+    download_mp3('https://www.youtube.com/watch?v=vrvs-Hy69oo')
